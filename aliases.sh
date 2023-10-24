1a8a44fa57ec34be77f1bf9d5e5c92c8d8bb5e8c
Bruno Vinicius <vruno.binicius@gmail.com>
2023-10-24 03:43:32 -0300

    zsh/aliases

--- aliases.sh
+++ aliases.sh
@@ -9,5 +9,8 @@ alias btd="bluetoothctl disconnect"
 alias mssh="ssh -i $HOME/dev/mandacaru/docs/vinci-api-key.pem ec2-user@ec2-18-228-126-185.sa-east-1.compute.amazonaws.com"
 
 # EyesOn API and WEB
-alias api_m="cd $HOME/dev/mandacaru/api; . ./venv/activate; flask run"
+alias api_m="cd $HOME/dev/mandacaru/api; source ./venv/bin/activate; flask run"
 alias web_m="cd $HOME/dev/mandacaru/web; pnpm dev"
+
+# Sinks
+alias shdmi="pactl set-default-sink $(pactl list sinks | grep -e 'Name:.*hdmi' | awk '{split($0, a, ": "); print a[2]}')"
