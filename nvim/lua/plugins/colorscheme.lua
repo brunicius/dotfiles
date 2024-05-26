8b14ca3c19e944a923a900d75d5987d71ec25f58
Bruno Vinicius <vruno.binicius@gmail.com>
2024-05-26 11:13:19 -0300

    nvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+-- return {
+--   "craftzdog/solarized-osaka.nvim",
+--   lazy = true,
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       transparent = true,
+--     }
+--   end,
+-- }
+return {
+  "diegoulloao/neofusion.nvim",
+  priority = 1000,
+  config = true,
+  opts = function()
+    return {
+      transparent = true,
+    }
+  end,
+}
