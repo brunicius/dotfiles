913b569fd4f393beaf266d27c3902caf4b537ea7
Bruno Vinicius <vruno.binicius@gmail.com>
2024-07-16 19:06:25 -0300

    feat: created correct config for nvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    "diegoulloao/neofusion.nvim",
+    priority = 1000,
+    config = true,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+  {
+    "miikanissi/modus-themes.nvim",
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      no_italic = true,
+      term_colors = true,
+      transparent_background = true,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        },
+        dropbar = {
+          enabled = true,
+          color_mode = true,
+        },
+      },
+    },
+  },
+}
